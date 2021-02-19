d95e596b63490def6c20844a1c2b349815c0256d
Mubbashir Ali <mubbashirali35@gmail.com>
2021-02-20 00:08:48 +0500

    Fix: Actual Labels chart linked to the div on html page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Datafeed from './datafeed.js';
-import CumPNLDatafeed from './CumPNLDatafeed.js' 
-import { studyTemplate } from './ordersStudy.js';
+import CumPNLDatafeed from './CumPNLDatafeed.js'; 
+import ActualLabelsDatafeed from './ActualLabelsDatafeed.js';
+// import { studyTemplate } from './ordersStudy.js';
 
 window.tvWidget = new TradingView.widget({
     symbol: 'NVDA',
@@ -17,6 +18,15 @@ window.tvWidget = new TradingView.widget({
     fullscreen: true, // displays the chart in the fullscreen mode
     container_id: 'cum_pnl_chart_container',
     datafeed: CumPNLDatafeed,
+    library_path: '../charting_library_clonned_data/charting_library/',
+});
+
+window.tvWidget = new TradingView.widget({
+    symbol: 'NVDA',
+    interval: '1', // default symbol // default interval
+    fullscreen: true, // displays the chart in the fullscreen mode
+    container_id: 'actual_labels_chart_container',
+    datafeed: ActualLabelsDatafeed,
     library_path: '../charting_library_clonned_data/charting_library/',
 });
 
